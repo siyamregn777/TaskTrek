d4e2bc208eda0acfbff76bd15b7e12bc942c61ee
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-03-18 23:43:13 +0300

    add Profile component to display user profile information

---
+++ tasktrek-frontend/src/views/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="profile">
+      <h1>User Profile</h1>
+      <!-- Profile content goes here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Profile',
+  };
+  </script>
+  
+  <style scoped>
+  .profile {
+    padding: 20px;
+  }
+  </style>
+  
