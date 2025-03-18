25ad7da83ae4dde3dae688742b24f59781e2d415
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-03-18 23:39:43 +0300

    add Footer component with basic styling and copyright notice

---
+++ tasktrek-frontend/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <p>&copy; 2025 TaskTrek. All rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+footer {
+  text-align: center;
+  padding: 20px;
+  background-color: #333;
+  color: white;
+}
+</style>
