4354a4ec5d4d42f06431474ef51e5a60374e69d1
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-03-18 23:42:34 +0300

    add About component with basic information about TaskTrek

---
+++ tasktrek-frontend/src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="about">
+      <h1>About TaskTrek</h1>
+      <p>TaskTrek is a project management tool that helps teams collaborate efficiently.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About',
+  };
+  </script>
+  
+  <style scoped>
+  .about {
+    padding: 20px;
+  }
+  </style>
+  
